b0eb3e29ef25af9f5ff14ee1d8f34534570440c1
DimiS0 <24132721@student.hhs.nl>
2026-03-11 12:20:44 +0100

    basis structuur

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+    public void main(String[] args) {
+
+        // object spel aanmaken en starten van het spel
+        Simulator simulator = new Simulator();
+        simulator.start();
+    }
+}
