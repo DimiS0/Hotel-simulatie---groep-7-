46cf934bb2dd4e916b02d014cc86efe3ab7f44c2
DimiS0 <24132721@student.hhs.nl>
2026-03-17 10:46:23 +0100

    Layout kiezer tevoorschijn

--- src/Simulator/HoofdSimulator.java
+++ src/Simulator/HoofdSimulator.java
@@ -1,15 +1,18 @@
 package Simulator;
 
+import javax.swing.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
+
 public class HoofdSimulator {
     private StarterGui swingGui;
     private Evenement event;
     private HTE hte;
 
     public HoofdSimulator() {
-        swingGui = new StarterGui();
+        swingGui = new StarterGui(this);
     }
 
 public void start() {
@@ -17,23 +20,38 @@ public void start() {
     Hotel hotel = new Hotel();
 
     swingGui.guiStart();
-    System.out.println("Voer een naam van een .layout bestand in");
-    String bestandnaam = scanner.nextLine();
+}
 
-    File file = new File(bestandnaam);
+    public void LayoutKiezer(){
+        JFileChooser fileChooser = new JFileChooser();
+        System.out.println("Voer een naam van een .layout bestand in");
 
-    try {
-        Scanner fileScanner = new Scanner(file);
-        while (fileScanner.hasNextLine()){
-            String regel = fileScanner.nextLine();
-            System.out.println(regel);
+        int result = fileChooser.showOpenDialog(null);
+
+        if(result != JFileChooser.APPROVE_OPTION){
+            System.out.println("geen bestand gekozen");
+            return;
         }
-        fileScanner.close();
+        File file = fileChooser.getSelectedFile();
 
-    } catch (FileNotFoundException e) {
-        System.out.println("Bestand niet gevonden: " + e.getMessage());
+        try {
+            Scanner fileScanner = new Scanner(file);
+            while (fileScanner.hasNextLine()) {
+                String regel = fileScanner.nextLine();
 
-         }
-    hotel.hotelLayout();
+                //regel vervangen met layout?
+                System.out.println(swingGui.layout);
+            }
+            fileScanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Bestand niet gevonden: " + e.getMessage());
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Het gekozen layoutbestand kan niet worden geladen.",
+                    "Fout",
+                    JOptionPane.ERROR_MESSAGE
+            );
+            return;
+        }
     }
 }
