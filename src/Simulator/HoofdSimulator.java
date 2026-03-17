64a04ace0d14f2a3ed99d393b6f2b2305c78a7ef
DimiS0 <24132721@student.hhs.nl>
2026-03-17 11:13:29 +0100

    comments bij LayoutKiezer

--- src/Simulator/HoofdSimulator.java
+++ src/Simulator/HoofdSimulator.java
@@ -23,28 +23,43 @@ public void start() {
 }
 
     public void LayoutKiezer(){
+
+        //maakt het bestand venster aan
         JFileChooser fileChooser = new JFileChooser();
         System.out.println("Voer een naam van een .layout bestand in");
 
+        //opent het venster, geeft terug wat de gebruiker heeft geclicked
         int result = fileChooser.showOpenDialog(null);
 
+        //als gebruiker op x klikt is result niet goedgekeurd
         if(result != JFileChooser.APPROVE_OPTION){
             System.out.println("geen bestand gekozen");
             return;
         }
+        //dit is het bestand dat de gebruiker gekozen heeft
         File file = fileChooser.getSelectedFile();
 
         try {
+            //opent het bestand met scanner om het te lezen
             Scanner fileScanner = new Scanner(file);
+
+            //elke regel lezen + print de regel
             while (fileScanner.hasNextLine()) {
                 String regel = fileScanner.nextLine();
 
-                //regel vervangen met layout?
+                //dit vervangen met code later
                 System.out.println(swingGui.layout);
             }
+
             fileScanner.close();
+
+            //als het bestand ongeldig is / niet kan geopend worden
         } catch (FileNotFoundException e) {
+
+            //Logt in de console waarom het mis ging
             System.out.println("Bestand niet gevonden: " + e.getMessage());
+
+            //messagebox: text is "het..geladen", de titel is: Fout, Error_Message zorgt voor rood icoontje
             JOptionPane.showMessageDialog(
                     null,
                     "Het gekozen layoutbestand kan niet worden geladen.",
