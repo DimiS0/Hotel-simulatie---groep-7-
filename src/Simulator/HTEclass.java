c4af5fa92a400019df6cbb7d1a6a9b2d8a15d7f7
msonmezdag111-alt <msonmezdag111@gmail.com>
2026-03-17 12:30:47 +0100

    HTE labels en factor

---
+++ src/Simulator/HTEclass.java
@@ -0,0 +1,13 @@
+package Simulator;
+public class HTEclass {
+
+    //zet label om naar HTE enum waarde
+    public HTE fromLabel(String label) {
+        for (HTE s : HTE.values()) {
+            if (s.getLabel().equalsIgnoreCase(label)) {
+                return s;
+            }
+        }
+        throw new IllegalArgumentException("Ongeldige snelheid: " + label);
+    }
+}
